b25936c7cac1b1bb6248363da912efacaf857c40
elcot <elcot@DESKTOP-CSTGC2T>
2022-01-23 03:41:00 -0500

    fifthproject

---
+++ 11-01-2022/src/com/javaVariables/Examaple1.java
@@ -0,0 +1,27 @@
+package com.javaVariables;
+
+public class Examaple1 {
+
+	public static void main(String[] swathi) 
+	{
+     float mobileprice = 13000;
+     String colour = "Blue";
+     String model = "7c";
+     String brand = "Honour";
+     String memory = "64GB";
+     int emeinumber = 345678;
+     
+     System.out.println(mobileprice);
+     System.out.println(colour);
+     System.out.println(model);
+     System.out.println(brand);
+     System.out.println(memory);
+     System.out.println(emeinumber);
+     
+     
+     
+     
+
+	}
+
+}
